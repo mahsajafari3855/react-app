0e6d271790a5809eea283068ac7776d0f3e48fea
M.jafari <mahsa.jafari38555@gmail.com>
2021-12-20 23:24:17 +0330

    add routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, {Component} from "react";
+import {Route,Routes} from "react-router-dom"
 
 import Navbar from "./components/Navbar";
-import Banner from "./components/Banner";
-import Cards from "./components/Cards";
-import Search from "./components/Search";
-import Logos  from "./components/Logos";
+import Products from "./components/Products";
+import Aboutus from "./components/Aboutus";
+
 import Footer from "./components/Footer";
+import Landing from "./components/Landing";
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Navbar />
-                <Banner />
-                <Cards />
-                <Search />
-                <Logos />
+                <Routes>
+                <Route path="/" exact element={<Landing/>}/>
+                <Route path="/products" element={<Products/>}/>
+                <Route path="/aboutus" element={<Aboutus/>}/>
+                </Routes>
+               
                 <Footer />
             </div>
         )
---
+++ src/components/Aboutus.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+
+
+class Aboutus extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Aboutus</h1>
+            </div>
+        )
+    }
+}
+
+export default Aboutus;
